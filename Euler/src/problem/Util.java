0223e76b4abc3f97472b233e890eef2ba72c09dd
yan96in <yan96in@163.com>
2017-02-15 08:37:48 +0800

    10

--- Euler/src/problem/Util.java
+++ Euler/src/problem/Util.java
@@ -8,13 +8,19 @@ package problem;
  *
  */
 public class Util {
+
+	public static void split(String s) {
+		String[] array = s.split(" ");
+		for (String st : array) {
+			System.out.print(st+",");
+		}
+	}
+
 	// 判断是否为质数
 	public static boolean isFactor(long num) {
 		for (int i = 2; i * i <= num; i++) {
-			if (num % i== 0)
+			if (num % i == 0)
 				return false;
-			else
-				continue;
 		}
 		return true;
 	}
@@ -33,9 +39,7 @@ public class Util {
 		String s = String.valueOf(object);
 		int len = s.length();
 		for (int i = 0; i <= len / 2; i++) {
-			if (s.charAt(i) == s.charAt(len - 1 - i))
-				continue;
-			else
+			if (s.charAt(i) != s.charAt(len - 1 - i))
 				return false;
 		}
 		return true;
@@ -48,15 +52,33 @@ public class Util {
 		if (num < 10000)
 			return false;
 		for (int i = 100; i < 1000; i++) {
-			int temp=num%i;
-			if (num%i==0) {
-				if(num/i>99&&num/i<1000){
-					System.out.println(num+"="+i + "*" + num / i);
+			int temp = num % i;
+			if (num % i == 0) {
+				if (num / i > 99 && num / i < 1000) {
+					System.out.println(num + "=" + i + "*" + num / i);
 					return true;
 				}
-			} else
-				continue;
+			}
 		}
 		return false;
+	}
+
+	// 计算一个数字中相连几个数字相乘所得积的最大值
+	public static long adjacentDigitProduct(String s, int digit) {
+		long maxProduct = 0;// 必须是long型的，int型的取值范围是正负2的31次方，肯定小于题目中的范围（9的13次方）
+		for (int i = 0; i <= s.length() - digit; i++) {
+			long product = 1;
+			for (int j = i; j < i + digit; j++) {
+				product *= Long.parseLong(String.valueOf(s.charAt(j)));
+				if (product == 0)
+					break;// 如果积已经为0，直接跳出循环
+				// System.out.println(s.charAt(j));
+			}
+			if (product > maxProduct) {
+				maxProduct = product;
+				// System.out.println(maxProduct);
+			}
+		}
+		return maxProduct;
 	}
 }
